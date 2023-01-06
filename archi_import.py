0eaa18c53cca6ffd11eba8d5fbc6d55f2c225633
Edvin Toome <edvin.toome@gmail.com>
2023-01-06 17:17:58 +0200

    Fix adding property to database

--- archi_import.py
+++ archi_import.py
@@ -26,10 +26,10 @@ def get_all_children(children, element):
             get_all_children(children, child)
     return children
 
-def add_data(sid, type, name, ipv4, os, vpc, stage, eol):
+def add_data(sid, type, name, ipv4, os, vpc, stage, eol, ext_ipv4, vendor, url):
 #    print(sid,type,name,ipv4,os,vpc,stage,eol)
     try: 
-        cur.execute("INSERT INTO archi_import (sid, type, name, ipv4, os, vpc, stage, eol) VALUES (?, ?, ?, ?, ?, ?, ?, ?)", (sid, type, name, ipv4, os, vpc, stage, eol)) 
+        cur.execute("INSERT INTO archi_import (sid, type, name, ipv4, os, vpc, stage, eol, ext_ipv4, vendor, url) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)", (sid, type, name, ipv4, os, vpc, stage, eol, ext_ipv4, vendor, url)) 
     except mariadb.Error as e: 
         print(f"Error adding data to MariaDB: {e}")
     conn.commit() 
@@ -51,14 +51,14 @@ for child in children:
     if type == "RealizationRelationship" or type == "AssignmentRelationship" or type == "ServingRelationship" or type == "AssociationRelationship" or type == "Connection" or type == "DiagramObject": 
         continue
 
-    property_value = [None, None, None, None, None]
-    property_key = ["IPv4", "OS", "VPC", "Stage", "EOL"]
+    property_value = [None, None, None, None, None, None, None, None]
+    property_key = ["IPV4", "OS", "VPC", "Stage", "EOL", "Ext_IPv4", "Vendor", "URL"]
     for property in child:
         for i in range(len(property_key)):
             if(property.get("key") == property_key[i]):
                 property_value[i] = (str(property.get("value")))
     
-    add_data(id, type, name, property_value[0], property_value[1], property_value[2], property_value[3], property_value[4])
+    add_data(id, type, name, property_value[0], property_value[1], property_value[2], property_value[3], property_value[4], property_value[5], property_value[6], property_value[7])
     x+=1
 print(x)
 
